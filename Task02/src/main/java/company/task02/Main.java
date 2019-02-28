59cbbea554974a6d5c44b9a000017a6fd049bb0a
Siarhei Kulik <siarhei.kulik84@gmail.com>
2019-02-28 13:25:51 +0300

    add Task02

---
+++ Task02/src/main/java/company/task02/Main.java
@@ -0,0 +1,9 @@
+package company.task02;
+
+public class Main {
+    public static void main(String[] args) {
+        for( int i = args.length - 1 ; i >= 0 ; i--)
+            System.out.println("Reverse : " + args[i]);
+
+    }
+}
